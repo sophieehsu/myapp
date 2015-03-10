7dfbb15d0851fb762606cc65cb4a8213f898063e
sophiee <hsfehsu@sce.pccu.edu.tw>
2015-03-10 15:18:19 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/sophieehsu/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145810,
+  277980,
+  385,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T06:55:17.471Z",
+  "2015-03-10T06:55:17.470Z",
+  "2015-03-10T06:55:17.579Z",
+  "2015-03-10T06:56:22.082Z",
+  "2015-03-10T06:55:17.511Z",
+  "2015-03-10T06:55:17.514Z",
+  "2015-03-10T06:55:17.466Z",
+  "2015-03-10T06:55:17.427Z",
+  "2015-03-10T06:55:30.013Z"
+ ],
+ "date": "2015-03-10T06:56:23.027Z"
+})
